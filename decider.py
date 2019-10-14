eaf2efeb383c433a6c32427010084f74b447d941
murklynx <56507407+murklynx@users.noreply.github.com>
2019-10-15 01:00:00 +0530

    Add files via upload

--- decider.py
+++ decider.py
@@ -1,90 +1,124 @@
-import pyttsx3, csv
+import pyttsx3
 import speech_recognition as sr
 import re
 import pandas as pd
 from command import *
+import requests
 
 global regex_data
 
 regex_data = pd.read_csv("regex.csv")
 
-from nmtchatbot.modded_inference import *
-
-nmt_chatbot("First response")
-
-
-
-
 engine= pyttsx3.init('sapi5')
 voices= engine.getProperty('voices')
-engine.setProperty('voices',voices[1].id)
+#print(voices[0].id)
+engine.setProperty('voices',voices[0].id)
+
 def speak(audio):
-    engine.say(audio)                               #Speaks for the AI
+    engine.say(audio)
     engine.runAndWait()
+
+#new yet to be embedded opens mic and listens to calls...(be safe)
+def auto_listen():
+
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("listening....")
+        while True:
+            audio = r.adjust_for_ambient_noise(source)
+            #r.pause_threshold = 1
+            audio = r.listen(source)
+            try:
+                query = r.recognize_google(audio,language='en-in')
+                query = query.lower()
+                print(query)
+                if "yo yo" in query:
+                    speak("yes tell me")
+                    decide_speak()
+            except:
+                pass
+
+
 
 def takecommand():
     '''take input as speech and
     gives string in return'''
-    r = sr.Recognizer()                             #Take input from user
-    with sr.Microphone() as source:                 #and converts into string
-        speak("speak what you want")
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
         print("listening.....")
         audio = r.adjust_for_ambient_noise(source)
         r.pause_threshold = 1
-        audio = r.listen(source, timeout=5, phrase_time_limit=10)
-        with open("audio.wav", "wb") as f:
-            f.write(audio.get_wav_data())
+        r.energy_threshold = 300
+        audio = r.listen(source, phrase_time_limit=10)
     try:
         print("recognizing...")
         query = r.recognize_google(audio,language='en-in')
-        speak("please wait")
-        print(f"user said: {query}\n")
-
+        #speak("please wait")
+        #print(f"user said: {query}\n")
+        return query
     except Exception as e:
 
         print("say that again please...")
         return None
-    #query = "open spotify"
-    return query
 
-def decide():
+def decide_type(query):
+    query = query.lower()
+    response = None
+    # query = 'Start camera'
+    # exit()
+    if query is not None:
+        # speak(query)
+        for row, key in zip(regex_data["regex"], regex_data["key"]):
+            if re.match(row, query) is not None:
+                if re.match(row, query).group() == query:
+                    response = command_caller(key, query)
+                    #speak(response)
+                    return response
+
+
+                ############################link to server########################
+        if response is None:
+            try:
+                reply = requests.post("http://192.168.43.204:8000/chatbot/",data= query , timeout= 2.5)
+                reply = reply.text
+            except:
+                reply = "The Internet and I are not talking right now."
+            #print(reply)
+            #speak(reply)
+            return reply
+    return "The Internet and I are not talking right now."
+
+def decide_speak():
     query = takecommand()
-    #query = "what is the color of the sky"
-
+    #query = 'Start camera'
+    #exit()
     if query is not None:
         query = query.lower()
-        speak(query)
+        response = None
+        #speak(query)
 
-##########################################################################################################################################
-                                            #Decider checks for the type of command in regex
-##########################################################################################################################################
-        response  = None
 
         for row, key in zip(regex_data["regex"],regex_data["key"]):
             if re.match(row, query) is not None:
                 if re.match(row, query).group() == query:
-                    response = command_caller(key)
-                    print(response)
-                    speak(response)
+                    response = command_caller(key, query)
+                    #speak(response)
+                    return response
+        ###################link to server###########################
         if response is None:
-            reply = nmt_chatbot(query)
-            print(reply)
-            speak(reply.split("::")[0])
+            try:
+                reply = requests.post("http://192.168.43.204:8000/chatbot/", data=query , timeout=2.5)
+                reply = reply.text
+            except:
+                reply = "The Internet and I are not talking right now."
+                # print(reply)
+                # speak(reply)
+            reply = {'reply' : reply, 'request' : query}
+            return reply
+    return "The Internet and I are not talking right now."
 
-if __name__ == "__main__":
-    while True:
-        if(input("Speak?")=="Y"):
-            pos = decide()
-        else:
-            exit()
-
-
-
-
+#if __name__ == "__main__":
 
 
-
-
-
-
-
+#auto_listen()
+#decide_speak()
