f074cdc3f0c17de59c0f2aeed9faf110f99cc681
Amartya Parijat <amartya4256@gmail.com>
2019-10-17 14:17:11 +0530

    Auto Listen done

--- Front end/script.js
+++ Front end/script.js
@@ -1,13 +1,28 @@
-autolisten();
+
+
+get_saved_state();
+
 var today = new Date();
-var time = today.getHours() + ":" + today.getMinutes();
+var time = today.getHours() + ":" + (today.getMinutes() < 10 ? '0' : '' ) + today.getMinutes();
+
+
+/* Open when someone clicks on the span element */
+function openNav() {
+    document.getElementById("myNav").style.height = "100%";
+    get_saved_state();
+}
+
+/* Close when someone clicks on the "x" symbol inside the overlay */
+function closeNav() {
+    document.getElementById("myNav").style.height = "0%";
+}
 
 
 
 function senddata(){
 
 	var today = new Date();
-	var time = today.getHours() + ":" + today.getMinutes();
+	var time = today.getHours() + ":" + (today.getMinutes() < 10 ? '0' : '') + today.getMinutes();
 	var msg = new SpeechSynthesisUtterance();
   	var voices = window.speechSynthesis.getVoices();
   	msg.voice = voices[0];
@@ -42,7 +57,7 @@ function senddata(){
 function speakdata(){
 
 	var today = new Date();
-	var time = today.getHours() + ":" + today.getMinutes();
+	var time = today.getHours() + ":" + (today.getMinutes() < 10 ? '0' : '') + today.getMinutes();
 	var msg = new SpeechSynthesisUtterance();
   	var voices = window.speechSynthesis.getVoices();
   	msg.voice = voices[0];
@@ -85,6 +100,9 @@ function speakdata(){
     
 
 function enterlistener(query){
+
+    /*var today = new Date();
+        var time = today.getHours() + ":" + (today.getMinutes() < 10 ? '0' : '' ) + today.getMinutes();*/
 	
 	if(event.key === 'Enter'){
 
@@ -113,37 +131,74 @@ function scroller(elem){
 
 
 
-function autolisten() {
+//creatingbuttonactions
+function doalert(changeelem){
 
-console.log("chalta hai");
-
-
-var SpeechRecognition = window.webkitSpeechRecognition;
-
-var recognition = new SpeechRecognition();
- console.log(recognition);
-var Content = '';
-
-recognition.continuous = true;
-
-recognition.onresult = function(event) {
-
-  var current = event.resultIndex;
-
-  var transcript = event.results[current][0].transcript;
-
-    Content = transcript;
-    if(Content.includes("time to talk")){
-	speakdata();
-}
-console.log("hi" + Content);
-
-};
-
-recognition.onerror = function() {
-  if(event.error == 'no-speech') {
-  }
+    if(changeelem.checked){
+        autolisten(0);
+        var x = new XMLHttpRequest();
+        x.onreadystatechange = function() {};
+        x.open("POST", "http://127.0.0.1:5000/autolisten/1");
+        x.send();
+    }
+    else{
+        autolisten(1);
+        var x = new XMLHttpRequest();
+        x.onreadystatechange = function() {};
+        x.open("POST", "http://127.0.0.1:5000/autolisten/0");
+        x.send();
+    }
 }
 
-recognition.start();
+function autolisten(checker) {
+
+    var today = new Date();
+    var time = today.getHours() + ":" + (today.getMinutes() < 10 ? '0' : '' ) + today.getMinutes();
+    var SpeechRecognition = window.webkitSpeechRecognition;
+    var recognition = new SpeechRecognition();
+    console.log(recognition);
+    var Content = '';
+    recognition.continuous = true;
+    recognition.onresult = function(event) {
+
+        var current = event.resultIndex;
+        var transcript = event.results[current][0].transcript;
+        Content = transcript;
+        if(Content.includes("time to talk")){
+	        speakdata();
+	        console.log("hi" + Content);
+        }
+    };
+
+    recognition.onerror = function() {
+        if(event.error == 'no-speech') {
+        }
+    }
+    recognition.start();
+    console.log("shuru");
+    if(checker == 1){
+        console.log("band");
+        recognition.stop();
+    }
+}
+
+
+function get_saved_state(){
+    var x = new XMLHttpRequest();
+    x.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            if(this.responseText=="1"){
+            console.log(document.getElementsByClassName("state")[0]);
+            autolisten(0);
+            console.log("activated");
+            try{
+            document.getElementById("state").checked = true;
+            console.log(document.getElementById("state").checked);
+            }
+            catch {}
+            }
+        }
+    }
+    x.open("GET","http://127.0.0.1:5000/autolisten/getdata");
+    x.send();
 }
